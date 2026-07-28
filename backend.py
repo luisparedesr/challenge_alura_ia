148a5fdd20504b8a7aea477fe955460dab8133c9
luisparedesr <luisparedes2025@gmail.com>
2026-07-27 21:19:14 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -27,11 +27,11 @@ def cargar_base_predeterminada():
         return
 
     # Obtiene la ruta de la carpeta raíz del proyecto
-        ruta_actual = os.path.dirname(os.path.abspath(__file__))
+    ruta_actual = os.path.dirname(os.path.abspath(__file__))
+    ruta_raiz = os.path.dirname(ruta_actual)
 
-        ruta_pdf = os.path.join(
-        ruta_actual,
-        "terminosycondiciones.pdf")
+    # Ruta del PDF
+    ruta_pdf = os.path.join(ruta_raiz, "documento", "terminosycondiciones.pdf")
 
     if os.path.exists(ruta_pdf):
         try:
